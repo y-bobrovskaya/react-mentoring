af6812cbfb4f680eccb00d4751e82a12b764c3f4
y-bobrovskaya <yana55588@mail.ru>
2017-09-09 17:06:36 +0300

    changing config for bundling the server file

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,20 @@
+var webpack = require('webpack');
 const path = require('path');
+var fs = require('fs');
+var nodeModules = {};
+
+// note the path.resolve(__dirname, ...) part
+// without it, eslint-import-resolver-webpack fails
+// since eslint might be invoked with different cwd
+fs.readdirSync(path.resolve(__dirname, 'node_modules'))
+    .filter(x => ['.bin'].indexOf(x) === -1)
+    .forEach(mod => { nodeModules[mod] = `commonjs ${mod}`; });
 
 module.exports = {
+	name: 'server',
+	target: 'node',
 	context: path.resolve(__dirname, 'src'),
-	entry: {
-		client: './client/app',
-		server: './server/server'
-	},
+	entry: './server/server',
 	output: {
 		filename: '[name].bundle.js',
 		path: path.resolve(__dirname, 'built'),
@@ -17,8 +26,14 @@ module.exports = {
 		rules: [
 			{
 				test: /\.jsx?$/,
-				exclude: [/node_modules/],
-				loader: 'babel-loader'
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						plugins: ["transform-react-jsx"],
+						presets: ['env']
+					}
+				}
 			}
 		]
 	},
